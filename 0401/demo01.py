60d2549b326ed6b555e41baa5e82b3c5cae38176
zhuxinan <18238955523qq>
2019-04-01 17:51:03 +0800

    python 高级语法

--- 0401/demo01.py
+++ 0401/demo01.py
@@ -1,1 +1,13 @@
-print("python 高级")
+"""
+python高级：属性与方法
+"""
+
+class Good():
+    """
+    动态的添加属性方法
+    """
+    def __init__(self,_name):
+        self.name = _name
+
+a1 = Good("shanghai")
+print(a1.name)
